a6589baea458472eb722a936353a3d471af0b569
James <traitor.cs@gmail.com>
2010-09-12 10:07:46 -0700

    Scripting branch

--- src/Location.java
+++
@@ -1,9 +0,0 @@
-/* Class used to store a location */
-
-public class Location {
-    public double x;
-    public double z;
-    public double y;
-    public float rotX;
-    public float rotY;
-}
--- src/Warp.java
+++
@@ -1,7 +0,0 @@
-
-public class Warp {
-
-    public int ID;
-    public String Name, Group;
-    public Location Location;
-}
