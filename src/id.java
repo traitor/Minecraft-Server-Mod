f1fd5139ddbd58ba48777a024a2878b45262d5f9
Jim <jim_the_miller@hotmail.com>
2010-10-27 02:47:58 +1030

    Trying to figure out why my text editor and github are mismatched

--- src/id.java
+++ src/id.java
@@ -1154,8 +1154,6 @@ public class id extends ej
                 if (split.length == 1) {
                     msg(Colors.Rose + "Correct usage is: /spawnmob [name] <amount>");
                     return;
-                    		//2 tabs
-                        //4 spaces
                 }
                 if (!Mob.isValid(split[1])) {
                     msg(Colors.Rose + "Invalid mob. Name has to start with a capital like so: Pig");
@@ -1165,11 +1163,38 @@ public class id extends ej
                 if (split.length == 2) {
                     Mob mob = new Mob(split[1], getPlayer().getLocation());
                     mob.spawn();
-                } else if (split.length == 3) {
-                    for (int i = 0; i < Integer.parseInt(split[2]); i++) {
-                        Mob mob = new Mob(split[1], getPlayer().getLocation());
-                        mob.spawn();
-                    }
+                }  else if (split.length == 3) {
+                	try {
+                		int mobnumber = Integer.parseInt(split[2]);
+                   		for (int i = 0; i < mobnumber; i++) {
+                     		Mob mob = new Mob(split[1], getPlayer().getLocation());
+                        	mob.spawn();
+                    	}
+                	}
+                	catch (NumberFormatException nfe) {
+                		if(!Mob.isValid(split[2])) {
+                			msg(Colors.Rose + "Invalid mob name or number of mobs.");
+                    		msg(Colors.Rose + "Mob names have to start with a capital like so: Pig");
+                		} else {
+                     		Mob mob = new Mob(split[1], getPlayer().getLocation());
+                        	mob.spawn(new Mob(split[2]));
+                		}
+                	}
+                } else if (split.length == 4) {
+                	try {
+                		int mobnumber = Integer.parseInt(split[3]);
+                		if(!Mob.isValid(split[2])) {
+                			msg(Colors.Rose + "Invalid rider. Name has to start with a capital like so: Pig");
+                		} else {
+                   			for (int i = 0; i < mobnumber; i++) {
+                     			Mob mob = new Mob(split[1], getPlayer().getLocation());
+                        		mob.spawn(new Mob(split[2]));
+                    		}
+                		}
+                	}
+                	catch (NumberFormatException nfe) {
+                			msg(Colors.Rose + "Invalid number of mobs.");
+                	}
                 }
             } else if (split[0].equalsIgnoreCase("/clearinventory")) {
                 Player target = getPlayer();
