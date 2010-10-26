6892c66d46e451a73f86de4869bc90e9d8debcfc
Jim <jim_the_miller@hotmail.com>
2010-10-27 03:27:40 +1030

    Trying to figure out why my text editor and github are mismatched

--- src/id.java
+++ src/id.java
@@ -1172,14 +1172,14 @@ public class id extends ej
                     		mob.spawn();
                     	}
                     } catch (NumberFormatException nfe) {
-                				if(!Mob.isValid(split[2])) {
-                						msg(Colors.Rose + "Invalid mob name or number of mobs.");
-                						msg(Colors.Rose + "Mob names have to start with a capital like so: Pig");
-                				} else {
-                						Mob mob = new Mob(split[1], getPlayer().getLocation());
-                						mob.spawn(new Mob(split[2]));
-                				}
-                		}
+                    	if(!Mob.isValid(split[2])) {
+                    		msg(Colors.Rose + "Invalid mob name or number of mobs.");
+                    		msg(Colors.Rose + "Mob names have to start with a capital like so: Pig");
+                    	} else {
+                    			Mob mob = new Mob(split[1], getPlayer().getLocation());
+                    			mob.spawn(new Mob(split[2]));
+                    	}
+                    }
                 }
                 
                 if (split.length == 4) {
