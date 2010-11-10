f86e1b11296be574ee93411a095bee5532f68b8b
Erik Broes <erikbroes@grum.nl>
2010-11-11 01:11:44 +0800

    bw -> by

--- src/by.java
+++ src/by.java
@@ -3,22 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import net.minecraft.server.MinecraftServer;
 
-public class by extends Thread
-{
-  public by(MinecraftServer paramMinecraftServer)
-  {
-  }
+public class by extends Thread {
+    private MinecraftServer server;
 
-  public void run()
-  {
-    BufferedReader localBufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    String str = null;
-    try {
-      while ((!this.a.g) && (MinecraftServer.a(this.a)) && ((str = localBufferedReader.readLine()) != null))
-        this.a.a(str, this.a);
-    }
-    catch (IOException localIOException) {
-      localIOException.printStackTrace();
-    }
-  }
+	public by(MinecraftServer paramMinecraftServer) {
+        this.server = paramMinecraftServer;
+	}
+
+	public void run() {
+		BufferedReader localBufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		String str = null;
+		try {
+            while (!server.g && (str = localBufferedReader.readLine()) != null) {
+                if (!etc.getInstance().parseConsoleCommand(str, server)) {
+                    server.a(str, server);
+                }
+            }
+		} catch (IOException localIOException) {
+			localIOException.printStackTrace();
+		}
+	}
 }
