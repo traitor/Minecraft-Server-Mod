33d5074bff1c5b5234feb86aac8989a2e0164f7b
Erik Broes <erikbroes@grum.nl>
2010-11-11 01:26:39 +0800

    gc -> ge

--- src/ge.java
+++ src/ge.java
@@ -1,28 +1,42 @@
+import java.io.File;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ge
-{
-  public static Logger a = Logger.getLogger("Minecraft");
+public class ge {
+    public static Logger a = Logger.getLogger("Minecraft");
 
-  public static void a() {
-    hs localhs = new hs();
+    public static void a() {
+        hs localhs = new hs();
 
-    a.setUseParentHandlers(false);
+        a.setUseParentHandlers(false);
 
-    ConsoleHandler localConsoleHandler = new ConsoleHandler();
-    localConsoleHandler.setFormatter(localhs);
-    a.addHandler(localConsoleHandler);
-    try
-    {
-      FileHandler localFileHandler = new FileHandler("server.log");
-      localFileHandler.setFormatter(localhs);
-      a.addHandler(localFileHandler);
-    } catch (Exception localException) {
-      a.log(Level.WARNING, "Failed to log to server.log", localException);
+        ConsoleHandler localConsoleHandler = new ConsoleHandler();
+        localConsoleHandler.setFormatter(localhs);
+        a.addHandler(localConsoleHandler);
+
+        // For parsers, or whatever.
+        try {
+            FileHandler localFileHandler = new FileHandler("server.log");
+            localFileHandler.setFormatter(localhs);
+            a.addHandler(localFileHandler);
+        } catch (Exception localException) {
+            a.log(Level.WARNING, "Failed to log to server.log", localException);
+        }
+
+        //Keep these files logged.
+        File log = new File("logs");
+        try {
+            if (!log.exists()) {
+                log.mkdir();
+            }
+            FileHandler localFileHandler = new FileHandler("logs/server_" + ((int) (System.currentTimeMillis() / 1000L)) + ".log");
+            localFileHandler.setFormatter(localhs);
+            a.addHandler(localFileHandler);
+        } catch (Exception localException) {
+            a.log(Level.WARNING, "Failed to log to server log", localException);
+        }
     }
-  }
 }
