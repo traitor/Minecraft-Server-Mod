651eeedae19fbbcd9a285586f0816fa91632fe47
traitor <traitor.cs@gmail.com>
2010-11-25 12:21:06 -0800

    Fixed class collision with runecraft (afaik)

---
+++ src/Digging.java
@@ -0,0 +1,22 @@
+/* Patch by Zeerix */
+public class Digging extends jt {
+
+    public Digging(eo world) {
+        super(world);
+    }
+
+    public Digging(eo world, er player) {
+        this(world);
+        a = player;
+    }
+
+    public boolean c(int x, int y, int z) {
+        Block block = etc.getServer().getBlockAt(x, y, z);
+        Boolean result = (Boolean) etc.getLoader().callHook(
+                PluginLoader.Hook.BLOCK_BROKEN, new Object[]{(ep) a, block});
+        if (result) {
+            return true;
+        }
+        return super.c(x, y, z);
+    }
+}
