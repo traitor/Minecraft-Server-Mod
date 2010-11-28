ff3f161fa0a8ca873e5ef38e8ce8089c794089bb
lightweight <lightweight@lightweight-desktop.(none)>
2010-11-28 17:27:15 +0100

    Adds addToQueue in Server.java as a wrapper for gt.add, to keep plugin-creators from having to change their code if the name of gt is changed.

--- src/Server.java
+++ src/Server.java
@@ -427,4 +427,14 @@ public class Server {
     public void updateBlockPhysics(int x, int y, int z, int data) {
         server.e.b(x, y, z, data);
     }
+    
+    /**
+     * Adds a runnable to the Server Queue, so that it will be executed in the Server Thread.
+     *  
+     * @param r - the runnable
+     * @return true if successful
+     */
+    public void addToServerQueue(Runnable r) {
+    	gt.add(r);
+    }    
 }
