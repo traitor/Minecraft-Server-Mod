aed870d04f0b75cb5670e40068b8013530ab08d0
mbaxter <github@phozop.net>
2010-11-29 17:20:08 -0500

    Adds auto-heal=true/false/default to server.properties By default, no change for the server If true, will always auto-heal, even with monsters If false, will never auto-heal, even without monsters

--- src/etc.java
+++ src/etc.java
@@ -45,6 +45,7 @@ public class etc {
     private PluginLoader loader;
     private boolean logging = false;
     private boolean enableHealth = true;
+    private PluginLoader.HookResult autoHeal = PluginLoader.HookResult.DEFAULT_ACTION;
     private boolean showUnknownCommand = true;
     private String versionStr;
     private boolean tainted = true;
@@ -149,6 +150,13 @@ public class etc {
             spawnProtectionSize = properties.getInt("spawn-protection-size", 16);
             logging = properties.getBoolean("logging", false);
             enableHealth = properties.getBoolean("enable-health", true);
+            String autoHealTemp = properties.getString("auto-heal", "default");
+            if(autoHealTemp.equalsIgnoreCase("true"))
+            	autoHeal=PluginLoader.HookResult.ALLOW_ACTION;
+            else if(autoHealTemp.equalsIgnoreCase("false"))
+            	autoHeal=PluginLoader.HookResult.PREVENT_ACTION;
+            else
+            	autoHeal=PluginLoader.HookResult.DEFAULT_ACTION;
             showUnknownCommand = properties.getBoolean("show-unknown-command", true);
             URL url = this.getClass().getResource("/version.txt");
             if (url != null) {
@@ -337,6 +345,16 @@ public class etc {
      */
     public boolean isHealthEnabled() {
         return enableHealth;
+    }
+    
+
+    /**
+     * Returns the status of auto-heal.
+     *
+     * @return
+     */
+    public PluginLoader.HookResult autoHeal() {
+        return autoHeal;
     }
 
     /**
