694b4cac77a663bab13954629858bc4d7b6f51c3
Dinnerbone <dinnerbone@dinnerbone.com>
2010-11-30 02:14:19 +0000

    Added Block: getFace, refresh, getRelative, isPowered, isIndirectlyPowered Added Server: isChunkLoaded, isBlockPowered, isBlockIndirectlyPowered

--- src/Block.java
+++ src/Block.java
@@ -248,4 +248,123 @@ public class Block {
     public void update() {
         etc.getServer().setBlock(this);
     }
+
+    /**
+     * Returns the block at the given Face
+     * 
+     * @param face the block face of which to return
+     * @return Block at the specified Face
+     */
+    public Block getFace(Face face) {
+        if (face == null) return null;
+
+        switch (face) {
+            case Front:
+                return getRelative(1, 0, 0);
+            case Back:
+                return getRelative(-1, 0, 0);
+            case Top:
+                return getRelative(0, 1, 0);
+            case Bottom:
+                return getRelative(0, -1, 0);
+            case Left:
+                return getRelative(0, 0, 1);
+            case Right:
+                return getRelative(0, 0, -1);
+        }
+
+        return null;
+    }
+
+    /**
+     * Synchronises this Block with the server, abandoning all local
+     * changes and refreshing the data with the current actual values
+     */
+    public void refresh() {
+        type = etc.getServer().getBlockIdAt(x, y, z);
+        data = etc.getServer().getBlockData(x, y, z);
+        status = 0;
+    }
+
+    /**
+     * Finds a Block relative to this Block
+     * 
+     * @param x amount to shift the x coordinate
+     * @param y amount to shift the y coordinate
+     * @param z amount to shift the z coordinate
+     * 
+     * @return Block at the requested location
+     */
+    public Block getRelative(int x, int y, int z) {
+        return etc.getServer().getBlockAt(this.getX() + x, this.getY() + y, this.getZ() + z);
+    }
+
+    /**
+     * Checks if this block is being powered through redstone
+     * 
+     * @return true if the block is being powered
+     */
+    public boolean isPowered() {
+        return etc.getServer().isBlockPowered(this);
+    }
+
+    /**
+     * Checks if this block is being indirectly powered through redstone
+     *
+     * @return true if the block is being indirectly powered
+     */
+    public boolean isIndirectlyPowered() {
+        return etc.getServer().isBlockIndirectlyPowered(this);
+    }
+
+    /**
+     * Returns a String value representing this Block
+     * 
+     * @return String representation of this block
+     */
+    @Override
+    public String toString() {
+        return String.format("Block[x=%d, y=%d, z=%d, type=%d]", x, y, z, type);
+    }
+
+    /**
+     * Tests the given object to see if it equals this object
+     * 
+     * @param obj the object to test
+     * @return true if the two objects match
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Block other = (Block) obj;
+        if (this.x != other.x) {
+            return false;
+        }
+        if (this.y != other.y) {
+            return false;
+        }
+        if (this.z != other.z) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Returns a semi-unique hashcode for this block
+     * 
+     * @return hashcode
+     */
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.x;
+        hash = 97 * hash + this.y;
+        hash = 97 * hash + this.z;
+        return hash;
+    }
 }
