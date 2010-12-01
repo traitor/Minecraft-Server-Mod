bb94ec766c776e3c2846e4c04f0e1d42c2a55201
Zeerix <phygon@gmx.de>
2010-12-01 22:59:24 +0100

    Restored RuneCraft compatibility.

--- src/Digging.java
+++ src/Digging.java
@@ -32,9 +32,23 @@ public class Digging extends ju {
      */
     public boolean c(int x, int y, int z) {
         Block block = etc.getServer().getBlockAt(x, y, z);
-        Boolean result = (Boolean) etc.getLoader().callHook(PluginLoader.Hook.BLOCK_BROKEN, new Object[]{(es) a, block});
-        if (result)
+        if ((Boolean) etc.getLoader().callHook(PluginLoader.Hook.BLOCK_BROKEN, new Object[]{(es) a, block})) {
             return true;
+        }
         return super.c(x, y, z);
+    }
+
+    /**
+     * Called when a player right-click air with an item in hand. We intercept it.
+     * @param player
+     * @param world
+     * @param item
+     * @return
+     */
+    public boolean a(fy player, ep world, hm item) {
+        if (player instanceof es && (Boolean) etc.getLoader().callHook(PluginLoader.Hook.ITEM_USE, new Object[]{((es)player).getPlayer(), new Item(item)})) {
+            return false;
+        }
+        return super.a(player, world, item);
     }
 }
