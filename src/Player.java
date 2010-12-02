6f9fcd1e7a9e877dd5df92176ff14c88c472badf
Erik Broes <erikbroes@grum.nl>
2010-12-02 15:08:14 +0100

    Remove useless constructor; shouldn't be used that way.

--- src/Player.java
+++ src/Player.java
@@ -30,15 +30,7 @@ public class Player extends LivingEntity {
     private Pattern badChatPattern = Pattern.compile("[^ !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\\[\\\\\\]^_'abcdefghijklmnopqrstuvwxyz{|}~\u2302\u00C7\u00FC\u00E9\u00E2\u00E4\u00E0\u00E5\u00E7\u00EA\u00EB\u00E8\u00EF\u00EE\u00EC\u00C4\u00C5\u00C9\u00E6\u00C6\u00F4\u00F6\u00F2\u00FB\u00F9\u00FF\u00D6\u00DC\u00F8\u00A3\u00D8\u00D7\u0192\u00E1\u00ED\u00F3\u00FA\u00F1\u00D1\u00AA\u00BA\u00BF\u00AE\u00AC\u00BD\u00BC\u00A1\u00AB\u00BB]");
 
     /**
-     * Creates a player interface
-     * @param player player to interface
-     */
-    public Player(es player) {
-        super(player);
-    }
-
-    /**
-     * Creates an empty player (FlatFileSource uses it, any use? O.o)
+     * Creates an empty player. Add the player by calling {@link #setUser(es)}
      */
     public Player() {
     }
