abc85d165b651fff4fe21304022acd382424620d
Erik Broes <erikbroes@grum.nl>
2010-12-05 19:42:49 +0100

    Fix lighters so they now will call the onItemUse hook, finally.

--- src/je.java
+++ src/je.java
@@ -9,6 +9,11 @@ public class je extends fw {
 
     @Override
     public boolean a(hn paramhn, fz paramfz, eq parameq, int paramInt1, int paramInt2, int paramInt3, int paramInt4) {
+
+        // hMod: Store block data clicked
+        Block blockClicked = new Block(parameq.a(paramInt1, paramInt2, paramInt3), paramInt1, paramInt2, paramInt3 );
+        blockClicked.setFaceClicked(Block.Face.fromId( paramInt4 ));
+
         if (paramInt4 == 0) {
             paramInt2--;
         }
@@ -31,20 +36,19 @@ public class je extends fw {
         int i = parameq.a(paramInt1, paramInt2, paramInt3);
 
         if (i == 0) {
-            // hMod: Hook to control ignites -- TODO This needs some looking over!
-            Block block = new Block(0, paramInt1, paramInt2, paramInt3);
-            block.setStatus(2);
-            // fire lighter check
-            if (!(Boolean) etc.getLoader().callHook(PluginLoader.Hook.IGNITE, block, ((et) paramfz).getPlayer())) {
-                // create fire effects ? (probably this)
-                parameq.a(paramInt1 + 0.5D, paramInt2 + 0.5D, paramInt3 + 0.5D, "fire.ignite", 1.0F, b.nextFloat() * 0.4F + 0.8F);
-                // set the block type to fire.
-                parameq.d(paramInt1, paramInt2, paramInt3, gc.ar.bh);
+            // hMod: Hook to control ignites AND ligther use
+            Block blockPlaced = new Block(Block.Type.Fire.getType(), paramInt1, paramInt2, paramInt3);
+            Player player = ((et) paramfz).getPlayer();
+
+            Boolean preventLighter = (Boolean) etc.getLoader().callHook(PluginLoader.Hook.ITEM_USE, player, blockPlaced, blockClicked, new Item(paramhn));
+            blockPlaced.setStatus(2); // Specificly to mark this ignite as from a lighter
+            Boolean preventIgnite = (Boolean) etc.getLoader().callHook(PluginLoader.Hook.IGNITE, blockPlaced, player);
+
+            if ( preventIgnite || preventLighter ) {
+                return false;
             } else {
-                // set block to glass.
-                parameq.d(paramInt1, paramInt2, paramInt3, 20);
-                // set block to air.
-                parameq.d(paramInt1, paramInt2, paramInt3, 0);
+                parameq.a(paramInt1 + 0.5D, paramInt2 + 0.5D, paramInt3 + 0.5D, "fire.ignite", 1.0F, b.nextFloat() * 0.4F + 0.8F);
+                parameq.d(paramInt1, paramInt2, paramInt3, gc.ar.bh);
             }
         }
 
