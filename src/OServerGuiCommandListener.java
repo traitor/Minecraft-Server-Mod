3b6acca7e30244fb67593f4493590b41fc91a0fc
Willem Mulder <willemmaster@hotmail.com>
2011-03-09 12:58:10 +0100

    Added GUI support, commands are parsed by hMod now

---
+++ src/OServerGuiCommandListener.java
@@ -0,0 +1,23 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JTextField;
+import net.minecraft.server.MinecraftServer;
+
+class OServerGuiCommandListener implements ActionListener {
+    final JTextField a;
+    final OServerGUI b;
+
+    OServerGuiCommandListener(OServerGUI paramOServerGUI, JTextField paramJTextField) {
+        a = paramJTextField;
+        b = paramOServerGUI;
+    }
+
+    public void actionPerformed(ActionEvent paramActionEvent) {
+        String str = a.getText().trim();
+        if (str.length() > 0 && etc.getMCServer() != null)
+            if (!etc.getInstance().parseConsoleCommand(str, etc.getMCServer()))
+                etc.getMCServer().a(str, b);
+        a.setText("");
+    }
+}
